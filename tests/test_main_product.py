9a5fffa62944c32d46f05df1cb142b199d83a308
Catrofe <lord.rico10@gmail.com>
2022-06-27 00:10:04 -0300

    TEST: Criando testes para o delete product

--- tests/test_main_product.py
+++ tests/test_main_product.py
@@ -13,11 +13,57 @@ def drop_database():
     asyncio.run(startup_event(True))
 
 
-"""
-Testes a realizar para o POST.
-Teste 1: Teste para verificar se o produto está sendo criado corretamente.
-Teste 2: Teste passando parametros inválidos.
-"""
+def register_user():
+    body = {
+        "email": "email@email.com",
+        "name": "Christian Lopes",
+        "cpf": "17410599090",
+        "password": "12345678"
+    }
+    response = client.post("/register/user", json=body)
+
+
+def login_user() -> str:
+    body = {
+        "login": "17410599090",
+        "password": "12345678",
+    }
+    response = client.post("/login/user", json=body)
+    return response.json()["token"]
+
+
+def register_employee():
+    body = {
+        "email": "email@email.com",
+        "name": "Christian Lopes",
+        "cpf": "17410599090",
+        "password": "12345678"
+    }
+    response = client.post("/register/employee", json=body)
+
+
+def login_employee() -> str:
+    body = {
+        "login": "17410599090",
+        "password": "12345678",
+    }
+    response = client.post("/login/employee", json=body)
+    return response.json()["token"]
+
+
+def create_product(token):
+    header = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": token,
+    }
+    body = {
+        "name": "Açai 200ml",
+        "description": "Açai 200ml",
+        "image_url": "http://www.google.com",
+        "price": "10,00"
+    }
+    response = client.post("/create/product", json=body, headers=header)
 
 
 def test_create_product_should_success(drop_database):
@@ -239,7 +285,53 @@ def test_update_product_price_should_success(drop_database):
     body = {"id": 1, "price": "11,99"}
     response = client.put("/update/product/1", json=body, headers=header)
 
-    print(response.text)
 
     assert response.status_code == 200
     assert response.json() == {"id": 1, "message": "UPDATE_PRODUCT_SUCCESS"}
+
+
+def test_delete_product_should_success(drop_database):
+    register_employee()
+    token = login_employee()
+    create_product(token)
+
+    header = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": token,
+    }
+    response = client.delete("/delete/product/1", headers=header)
+
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "message": "DELETE_PRODUCT_SUCCESS"}
+
+  
+def test_delete_product_nonexistent_should_fail(drop_database):
+    register_employee()
+    token = login_employee()
+    create_product(token)
+
+    header = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": token,
+    }
+    response = client.delete("/delete/product/2", headers=header)
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "PRODUCT_NOT_FOUND"}
+
+
+def test_delete_product_without_token_should_fail(drop_database):
+    register_user()
+    token = login_user()
+    create_product(token)
+
+    header = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    response = client.delete("/delete/product/1", headers=header)
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "ACCESS_DENIED"}
